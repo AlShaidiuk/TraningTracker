1800a5c76c2dad84605b42056dd79ab0bae02f53
AlShaidiuk <alexeishaidyuk@yandex.ru>
2026-05-31 16:06:51 +0300

    Create config.js

---
+++ firebase/config.js
@@ -0,0 +1,15 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB_xheiQv0WfSmghmELLS93-3k5Qaq4HEk",
+  authDomain: "traningtracker.firebaseapp.com",
+  projectId: "traningtracker",
+  storageBucket: "traningtracker.firebasestorage.app",
+  messagingSenderId: "959611040594",
+  appId: "1:959611040594:web:c19a438dc476845dc4008b",
+  measurementId: "G-Y6J4699NRR"
+};
+
+// Инициализация Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
